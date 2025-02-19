16e5b8f4efad5a29e8ce260acd0f182b74e8354a
unknown <williammacluskie@yahoo.co.uk>
2025-02-19 21:03:49 +0000

    Adding router functionality, early steps

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,36 @@
-import { useCallback, useEffect, useState } from "react"
-import styled from 'styled-components'
-import "./index.css"
+//import { useCallback, useEffect, useState } from "react"
+//import styled from 'styled-components'
 
+
+// Component imports
 import {JumboTron} from "./components/JumboTron"
-
-import Contact from "./components/Contact"
-import NavigationBar from "./components/NavigationBar"
+//import Contact from "./components/Contact"
+//import NavigationBar from "./components/NavigationBar"
 import { ProjectSection } from "./components/ProjectSection"
-import Skills from "./components/Skills"
+//import Skills from "./components/Skills"
 import Footer from "./components/Footer"
 
+// Utility imports
+import "./index.css"
+
+// Page imports 
 
 export default function App() { 
   return( 
-    <main >
-      <div className="mainPageContent">
-        {/* Nav bar will sit at top of page*/}
-        <div className="navbar">
-          {/* <NavigationBar /> */}
-        </div>
-        <JumboTron name="Will" age={27} job="developer?" buttonColor="red"/>
-        <ProjectSection />
-        {/* <Skills />
-        <Contact /> */}
-      </div>
-      <div className="footer">
-        <Footer /> 
-      </div>
-    </main>
+        <main >
+          <div className="mainPageContent">
+            {/* Nav bar will sit at top of page*/}
+            <div className="navbar">
+              {/* <NavigationBar /> */}
+            </div>
+            <JumboTron name="Will" age={27} job="developer?" buttonColor="red"/>
+            <ProjectSection />
+            {/* <Skills />
+            <Contact /> */}
+          </div>
+          <div className="footer">
+            <Footer /> 
+          </div>
+        </main>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import { BrowserRouter, Routes } from 'react-router'
+
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-  <div className="app">
-    <App />
-  </div>
-  </StrictMode>,
+    <BrowserRouter>
+        <Routes>
+            {/*TODO: REFER TO THIS PAGE IN REACT ROUTER https://reactrouter.com/start/library/routing AND CONTINUE FROM HERE */}
+            <App />
+
+        </Routes>
+      </BrowserRouter>
+  </StrictMode>
 )
