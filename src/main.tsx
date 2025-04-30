3b8d4edc6dcdf7130b7a2711c7cedf90874227f1
unknown <williammacluskie@yahoo.co.uk>
2025-04-30 21:55:08 +0100

    pain pain, fixing routing edge case for deployment I overlooked

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import { BrowserRouter, Routes, Route } from 'react-router'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-     <BrowserRouter>
+     <BrowserRouter basename={"/<home>/"}>
         <Routes> 
             {/*TODO: REFER TO THIS PAGE IN REACT ROUTER https://reactrouter.com/start/library/routing AND CONTINUE FROM HERE */}
             <Route path= "/" element={ <App />} />
