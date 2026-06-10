4258a712d353ceb89717fdc1a02bec5f8d216ba9
William Macluskie <william.macluskie@co-operativebank.co.uk>
2026-06-10 13:43:12 +0100

    temp removal of routes for testing

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import { BrowserRouter, Routes, Route } from 'react-router'
+import { BrowserRouter, Routes, /*Route*/ } from 'react-router'
 
 
 
@@ -10,8 +10,8 @@ createRoot(document.getElementById('root')!).render(
      <BrowserRouter basename={"/home"}>
         <Routes> 
             {/*TODO: REFER TO THIS PAGE IN REACT ROUTER https://reactrouter.com/start/library/routing AND CONTINUE FROM HERE */}
-            <Route path= "/" element={ <App />} />
-
+            {/* <Route path= "/" element={ <App />} /> */}
+         <App />
          </Routes>
       </BrowserRouter> 
   </StrictMode>
